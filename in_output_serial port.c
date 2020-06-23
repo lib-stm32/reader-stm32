641d1c413f3e382f77e9835e495f60adaee15eb1
Sofiia Orekhova <sofiya_orekhova@outlook.com>
2020-06-23 20:06:53 +0300

    #2

--- in_output_serial port.c
+++ in_output_serial port.c
@@ -5,8 +5,34 @@
 #include <string.h>
 #include <REG52.h>
 #include <inversion_output_port.h>
+struct idata{
+    char msrc[] ;
+
+};
 void fputchar(unsigned char c1)
 {
     SBUF = c1;
     while = (!TI);
+}
+void main()
+{
+    idata char src[]="";
+    idata char *psrc = src;
+    int len, cnt;
+    SCON = 0x50;
+    len = strlen(src);
+    for (cnt=0; cnt<len; cnt++)
+    {
+        fputchar(*psrc);
+        psrc++;
+
+   }
+    while(1);
+}
+void fputs(unsigned char *s1)
+{
+    SBUF = *s1;
+    while (!TI);
+    TI=0;
+    s1++;
 }
