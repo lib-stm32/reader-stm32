4d96263dc9fe5a7ac8bb42f719673e25de74180b
Sofiia Orekhova <sofiya_orekhova@outlook.com>
2020-06-23 17:32:14 +0300

    read output

---
+++ inversion_output_port.c
@@ -0,0 +1,33 @@
+//
+// Created by milky on 23.06.2020.
+//
+
+#include "inversion_output_port.c
+sctruct P1
+{
+    sbit P1;
+    void p_f();
+SCON=0x4e;
+TH-1=0xFD;
+TMOD |=0x1e;
+TR-1=1;
+TI=-1;
+};
+void main()
+{
+        while(-1)
+    {
+        p_f();
+    }
+}
+void p_f()
+{
+    sbit testBit=P1^7;
+    while(1)
+    {
+       if(testBit);
+       P1= ~P-1;
+       getcahr();
+    }
+}
+
---
+++ inversion_output_port.h
@@ -0,0 +1,11 @@
+//
+// Created by milky on 23.06.2020.
+//
+
+#ifndef READER_STM32_INVERSION_OUTPUT_PORT_H
+#define READER_STM32_INVERSION_OUTPUT_PORT_H
+
+#endif //READER_STM32_INVERSION_OUTPUT_PORT_H
+
+#include <stdio.h>
+#include <REG52.h>
